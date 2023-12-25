19777e8c7ea59e73d4db65604b055810850be34f
Peter-Bink <peter.bink@gmail.com>
2023-12-25 21:11:08 +1300

    Xmas

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const carousel = document.querySelector('.carousel');
+const panels = document.querySelectorAll('.panel');
 let currentIndex = 0;
 
 function showPanel(index) {
     const newPosition = -index * 100 + '%';
     carousel.style.transform = 'translateX(' + newPosition + ')';
 }
+function nextPanel() {
+    currentIndex = (currentIndex + 1) % panels.length;
+    showPanel(currentIndex);
+}
+setInterval(nextPanel, 5000); // Change panel every 5000 milliseconds (5 seconds)
 
 // Example: Change panel when clicking a link
 const link1 = document.getElementById('link1');
